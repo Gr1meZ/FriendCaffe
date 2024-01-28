fbc5f7015f952239611d70695f459481477f30f1
artyom <vegitoxxxgogeta@gmail.com>
2024-01-28 16:53:32 +0300

    Add migration

---
+++ FriendCaffe.Infrastructure/Migrations/20240128133451_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FriendCaffe.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email_Value = table.Column<string>(type: "text", nullable: false),
+                    Password_Hash = table.Column<string>(type: "text", nullable: false),
+                    Address_Country = table.Column<string>(type: "text", nullable: true),
+                    Address_Street = table.Column<string>(type: "text", nullable: true),
+                    Address_City = table.Column<string>(type: "text", nullable: true),
+                    UserDetails_Name = table.Column<string>(type: "text", nullable: false),
+                    UserDetails_Surname = table.Column<string>(type: "text", nullable: false),
+                    UserDetails_Nickname = table.Column<string>(type: "text", nullable: false),
+                    UserDetails_About = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
